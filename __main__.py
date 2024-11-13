512b8f6e9228e797e290f20bfc8e081cd4396d22
TheGreatLegend <salecha.aahan@gmail.com>
2024-11-13 22:32:16 +0530

    Added game screen

--- __main__.py
+++ __main__.py
@@ -1,17 +1,17 @@
 # ======= imports ======= #
 from customtkinter import *
-import os
+from os import path as osPath, makedirs, getlogin
 from sqlite3 import connect
 from sys import path
 from _tkinter import TclError
 from PIL import Image, ImageTk
+from random import randint, choice
 # ======= ------- ======= #
 
 # ======= constants ======= #
-USER = os.getlogin()
+USER = getlogin()
 DIRECTORY = f"C:\\Users\\{USER}\\AppData\\Local\\"+"the2048game\\"
 COLORS = [None, "#eee4da", "#ebd8b6", "#f3b178", "#f69562", "#f88165", "#f76644", "#f0d26c", "#edcc61", "#edc850", "#edc53f", "#edc22e", "#3c3a32"]
-TEXT_COLORS = ["#756452", "#ffffff"]
 PATHS = {
     "empty" : f"{path[0]}\\images\\empty.ico",
     "favicon" : f"{path[0]}\\images\\favicon.ico",
@@ -20,8 +20,13 @@ PATHS = {
 FONT = "JetBrains Mono Medium"
 # ======= --------- ======= #
 
+# ======= variables ======= #
+grid = [None]*16
+matrix = [0]*16
+# ======= --------- ======= #
+
 # ======= directory and database creation ======= #
-if not os.path.exists(DIRECTORY): os.makedirs(DIRECTORY)
+if not osPath.exists(DIRECTORY): makedirs(DIRECTORY)
 
 conn = connect(f"{DIRECTORY}\\2048.db")
 cursor = conn.cursor()
@@ -32,10 +37,11 @@ cursor.execute("CREATE TABLE IF NOT EXISTS Scores (Time TEXT, Name TEXT, Score I
 # ======= window ======= #
 class App(CTk):
 
+    # ======= init ======= #
     def __init__(root):
         # ======= window setup ======= #
         super().__init__("#faf8f0")
-        root.geometry("600x600+50+50")
+        root.geometry("600x700+50+50")
         root.resizable(False, False)
         # ======= ------ ----- ======= #
 
@@ -59,24 +65,24 @@ class App(CTk):
         # ======= variables ======= # 
         root.scoreVar = IntVar(master=root, value=0)
         root.highVar = IntVar(master=root, value=cursor.execute(f'''SELECT MAX(Score) FROM Scores''').fetchone()[0])
+        icon = ImageTk.PhotoImage(Image.open(PATHS["icon"]).resize((80, 80)))
         # ======= --------- ======= # 
 
         # ======= gui ======= #
-        root.icon = ImageTk.PhotoImage(
-            Image.open(PATHS["icon"]).resize((80, 80))
-        )
-
-        root.header = Header(root, root.scoreVar, root.highVar, root.icon)
+        root.header = Header(root, root.scoreVar, root.highVar, icon)
+        root.game = GameScreen(root)
         # ======= --- ======= #
 
         # ======= mainloop ======= #
         root.mainloop()
         # ======= -------- ======= #
+    # ======= ---- ======= #
 # ======= ------ ======= #
 
 # ======= header ======= #
 class Header(CTkFrame):
 
+    # ======= init ======= #
     def __init__(header, master, scoreVar, highVar, icon):
         # ======= setup ======= #
         super().__init__(
@@ -118,13 +124,15 @@ class Header(CTkFrame):
         # ======= --- ======= #
 
         # ======= place ======= #
-        header.place(x=300, y=60, anchor="center")
+        header.place(relx=0.5, y=60, anchor="center")
         # ======= ----- ======= #
+    # ======= ---- ======= #
 # ======= ------ ======= #
 
 # ======= name plate box ======= #
 class NamePlate(CTkFrame):
 
+    # ======= init ======= #
     def __init__(nameplate, master, icon):
         # ======= setup ======= #
         super().__init__(master, fg_color="transparent")
@@ -147,7 +155,7 @@ class NamePlate(CTkFrame):
             nameplate,
             text_color="#756452",
             text="2048",
-            font=(FONT, 50)
+            font=(FONT, 50, "bold")
         )
         nameplate.name.place(relx=0.6, rely=0.5, anchor="center")
         # ======= --- ======= #
@@ -155,12 +163,13 @@ class NamePlate(CTkFrame):
         # ======= place ======= #
         nameplate.grid(row=0, column=0, sticky="nsew", padx=10, pady=19)
         # ======= ----- ======= #
-
+    # ======= ---- ======= #
 # ======= ---- ----- --- ======= #
 
 # ======= score box ======= #
 class Score(CTkFrame):
 
+    # ======= init ======= #
     def __init__(score, master, color, pos, description, var):
         # ======= setup ======= #
         super().__init__(
@@ -173,7 +182,7 @@ class Score(CTkFrame):
         # ======= ----- ======= #
 
         # ======= gui ======= #
-        score.label = CTkLabel(score, text=description, font=(FONT, 10), text_color="#988a86")
+        score.label = CTkLabel(score, text=description, font=(FONT, 13), text_color="#988a86")
         score.label.place(relx=0.5, rely=0, anchor="n")
 
         score.number = CTkLabel(score, textvariable=var, text_color="#988a86", font=(FONT, 20))
@@ -183,7 +192,52 @@ class Score(CTkFrame):
         # ======= place ======= #
         score.grid(row=0, column=pos, sticky="nsew", padx=15, pady=19)
         # ======= ----- ======= #
+    # ======= ---- ======= #
 # ======= ----- --- ======= #
+
+# ======= game screen ======= #
+class GameScreen(CTkFrame):
+
+    # ======= init ======= #
+    def __init__(screen, master):
+        # ======= setup ======= #
+        super().__init__(
+            master,
+            fg_color="#9b8878",
+            corner_radius=35,
+            width=555, 
+            height=555, 
+        )
+        # ======= ----- ======= #
+
+        # ======= grid setup ======= #
+        screen.rowconfigure((0, 1, 2, 3), weight=1, uniform="a")
+        screen.columnconfigure((0, 1, 2, 3), weight=1, uniform="a")
+        screen.grid_propagate(False)
+        # ======= ---- ----- ======= #
+
+        # ======= base design ======= #
+        for row in range(4):
+            for column in range(4):
+                CTkLabel(
+                    screen, 
+                    text="",
+                    fg_color="#bdac97",
+                    corner_radius=35,
+                    width=116.25,
+                    height=116.25
+                ).place(
+                    anchor="center", 
+                    x=136.25*column+73.125,
+                    y=136.25*row+73.125
+                )
+        # ======= ---- ------ ======= #
+
+        # ======= place ======= #
+        screen.place(relx=0.5, y=400, anchor="center")
+        # ======= ----- ======= #
+    # ======= ---- ======= #
+# ======= ---- ------ ======= #
 
 # ======= main code ======= #
 if __name__ == "__main__":
